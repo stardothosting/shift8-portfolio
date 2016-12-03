c63e99d93f1f11e5e1fc8b5ec9661e6bbb997952
Kevin <shift8web@3057983.no-reply.drupal.org>
2016-12-03 01:53:33 -0500

    Added featherlight jquery gallery

--- components/enqueuing.php
+++ components/enqueuing.php
@@ -2,7 +2,14 @@
 
 // Load Front end CSS and JS
 function shift8_portfolio_scripts() {
-        wp_enqueue_style( 'shift8bootstrap', plugin_dir_url(dirname(__FILE__)) . 'bootstrap/css/bootstrap.css');
+	// Bootstrap 
+	wp_enqueue_style( 'shift8bootstrap', plugin_dir_url(dirname(__FILE__)) . 'bootstrap/css/bootstrap.min.css');
+	// Featherlight
+        wp_enqueue_style( 'shift8flcss', plugin_dir_url(dirname(__FILE__)) . 'css/featherlight.min.css');
+        wp_enqueue_style( 'shift8flgallerycss', plugin_dir_url(dirname(__FILE__)) . 'css/featherlight.gallery.min.css');
+        wp_enqueue_script( 'shift8fljs', plugin_dir_url(dirname(__FILE__)) . 'js/featherlight.min.js','','',true);
+        wp_enqueue_script( 'shift8flgalleryjs', plugin_dir_url(dirname(__FILE__)) . 'js/featherlight.gallery.min.js','','',true);
+	// Main CSS
         wp_enqueue_style( 'shift8portfolio', plugin_dir_url(dirname(__FILE__)) . 'css/shift8_portfolio.css');
 }
 add_action( 'wp_enqueue_scripts', 'shift8_portfolio_scripts', 12,1 );
--- template/single-portfolio.php
+++ template/single-portfolio.php
@@ -30,10 +30,11 @@ get_header(); ?>
 		$gallery_array = explode(',', get_post_meta(get_the_ID(),'shift8_portfolio_gallery',true));
 		if (is_array($gallery_array)) {
 			$gallery_display .= '<ul class="shift8-portfolio-gallery">';
+			$gallery_display .= '<section data-featherlight-gallery data-featherlight-filter="a">';
 			foreach ($gallery_array as $gallery_item) {
-				$gallery_display .= '<li><img id="shift8-portfolio-item-' . $gallery_item . '" src="' . wp_get_attachment_thumb_url($gallery_item) . '"></li>';
+				$gallery_display .= '<li><a href="' . wp_get_attachment_url($gallery_item) . '"><img id="shift8-portfolio-item-' . $gallery_item . '" src="' . wp_get_attachment_thumb_url($gallery_item) . '"></a></li>';
 			}
-			$gallery_display .= '</ul>';
+			$gallery_display .= '</section></ul>';
 		}
 
 
@@ -43,16 +44,6 @@ get_header(); ?>
         <!-- Client page -->
         <div class="row shift8-portfolio-single-row1">
             <div class="col-md-12">
-            <!--<p style="text-align: left;">
-            <div class="shift8-portfolio-back">
-            <svg version="1.1" id="Flash" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                 viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
-            <path d="M6.803,18.998c-0.194-0.127,3.153-7.16,3.038-7.469c-0.116-0.309-3.665-1.436-3.838-1.979
-                c-0.174-0.543,7.007-8.707,7.196-8.549c0.188,0.158-3.129,7.238-3.039,7.469c0.091,0.23,3.728,1.404,3.838,1.979
-                C14.109,11.024,6.996,19.125,6.803,18.998z"/>
-            </svg>
-            </div>
-            <span class="shift8-portfolio-back-heading"><a href="<?php echo get_site_url(); ?>/our_work">Back to Our Work Page</a></span></p>-->
             <p style="text-align: center;"><h1 class="shift8-portfolio-heading"><?php echo $client_name; ?></span></h1></p>
             <div class="shift8-portfolio-title-border" style="text-align: center;"></div>
             </div>
@@ -80,5 +71,15 @@ get_header(); ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 
+<script type='text/javascript'>
+jQuery(document).ready(function(){
+	jQuery('a.gallery').featherlightGallery({
+		previousIcon: '«',
+		nextIcon: '»',
+		galleryFadeIn: 300,
+		openSpeed: 300
+	});
+});
+</script>
 <?php
 get_footer();
